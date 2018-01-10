70e09bc34fc89715c88d461c5db3f31d70f2dbaa
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-01-10 15:30:21 +0100

    ingredients from recipe to shopping list - independent work

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Recipe} from "../recipe.model";
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,10 +8,13 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe = new Recipe("nothing", "nothing", "nothing")
-  constructor() { }
+  @Input() recipe: Recipe = new Recipe("nothing", "nothing", "nothing", [])
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
   }
 
+  addIngredientsToShoppingList(){
+    this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,16 +1,42 @@
-import { EventEmitter} from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipesService {
+    constructor(private shoppingListService: ShoppingListService) { };
+
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe("A Test Recipe", "This is simply a test",
-            `http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg`),
-        new Recipe("Pasta vegetale", "Delicious pasta!", "http://images.media-allrecipes.com/images/75131.jpg")
-    ]
+        new Recipe(
+            "Losos",
+            "Delicious and healthy",
+            "https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg",
+            [
+                new Ingredient("losos", 1),
+                new Ingredient("lemon", 2),
+                new Ingredient("rosemary", 4)
+            ]),
+        new Recipe(
+            "Pasta vegetale",
+            "Delicious pasta!",
+            "http://images.media-allrecipes.com/images/75131.jpg",
+            [
+                new Ingredient("pasta", 1),
+                new Ingredient("zuchini", 3),
+                new Ingredient("cherry tomato", 10),
+                new Ingredient("olive oil", 3)
+            ])
+    ];
 
-    getRecipes(){
+    getRecipes() {
         return this.recipes.slice();
     }
 
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        ingredients.forEach((ingredient) => {
+            this.shoppingListService.addIngredient(ingredient);
+        })
+    }
 }
